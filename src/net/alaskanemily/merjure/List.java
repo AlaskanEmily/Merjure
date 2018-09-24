b14e5db95fbc3f3488253ce9a1f2b2787c7f0ccc
AlaskanEmily <emily@alaskanemily.net>
2018-09-23 23:52:32 -0700

    Fix next()

--- src/net/alaskanemily/merjure/List.java
+++ src/net/alaskanemily/merjure/List.java
@@ -33,18 +33,18 @@ import clojure.lang.ISeq;
 public class List<E> implements ISeq {
     
     final private list.List_1<E> m_list;
+    static final private int INVALID_SIZE = -1;
     final private int m_size; // Minor optimization, we can store the current size here.
     
     // Used in the constructors to get the size of existing lists.
     // All other operations change this by a known amount.
-    static private <F> int calculateSize(list.List_1<F> a_list){
-        int size = 0;
-        list.List_1<F> counting_list = a_list;
+    private void calculateSize(){
+        m_size = 0;
+        list.List_1<E> counting_list = m_list;
         while(!list.is_empty(counting_list)){
-            size++;
+            m_size++;
             counting_list = list.det_tail(counting_list);
         }
-        return size;
     }
     
     private List(list.List_1<E> other_list, int initial_size){
@@ -59,11 +59,11 @@ public class List<E> implements ISeq {
     
     public List(list.List_1<E> other_list){
         m_list = other_list;
-        m_size = calculateSize(other_list);
+        m_size = INVALID_SIZE;
     }
     
     public ISeq seq() {
-        if(m_size == 0)
+        if(count() == 0)
             return null;
         else
             return this;
@@ -72,34 +72,39 @@ public class List<E> implements ISeq {
     public IPersistentCollection empty() { return new List<E>(); }
     
     public Object first(){
-        if(m_size == 0)
+        if(count() == 0)
             return null;
         else
             return list.det_head(m_list);
     }
     
     public ISeq more(){
-        if(m_size <= 1)
+        final ISeq s = next();
+        if(s == null)
             return new List<E>();
         else
-            return new List<E>(list.det_tail(m_list), m_size-1);
+            return new List<E>(list.det_tail(m_list), count()-1);
     }
     
     public ISeq next(){
-        if(m_size < 1)
+        if(count() <= 1)
             return null;
         else
-            return new List<E>(list.det_tail(m_list), m_size-1);
+            return new List<E>(list.det_tail(m_list), count()-1);
     }
     
     public ISeq cons(Object o){
-        if(m_size == 0)
+        if(count() == 0)
             return new List<E>(list.cons((E)o, list.empty_list()), 1);
         else
-            return new List<E>(list.cons((E)o, m_list), m_size+1);
+            return new List<E>(list.cons((E)o, m_list), count()+1);
     }
     
-    public int count() { return m_size; }
+    public int count() {
+        if(m_size == INVALID_SIZE)
+            calculateSize();
+        return m_size;
+    }
     
     public boolean equiv(Object other_sequence_o){
         
